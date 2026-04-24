32335584a727e241f8d2b7366c26521e326fe67d
livianawinata-2 <livianawinata2@gmail.com>
2026-04-24 15:30:08 +0700

    lengkapin routes.js

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const users = require('./components/users/user-route');
-const likes = require('./components/like/like-router');
-const follows = require('./components/follow/follow-router');
-const comments = require('./components/comment/comment-router');
-const dm = require('./components/dm/dm-route');
-const settings = require('./components/settings/settings-route');
 
-module.exports = () => {
-  const app = express.Router();
+const userRoute = require('./components/users/user-route');
+const postRoute = require('./components/post/post-route');
+const saveRoute = require('./components/save/save-route');
+const likeRoute = require('./components/like/like-router');
+const repostRoute = require('./components/repost/repost-router');
+const commentRoute = require('./components/comment/comment-router');
+const followRoute = require('./components/follow/follow-router');
+const dmRoute = require('./components/dm/dm-route');
+const settingsRoute = require('./components/settings/settings-route');
+const statusRoute = require('./components/status/status-router');
+const exploreRoute = require('./components/explore/explore-router');
 
-  app.use('/users', users);
-  app.use('/likes', likes);
-  app.use('/follows', follows);
-  app.use('/comments', comments);
-  app.use('/dm', dm);
-  app.use('/settings', settings);
+const router = express.Router();
 
-  return app;
-};
+router.use('/users', userRoute);
+router.use('/posts', postRoute);
+router.use('/save', saveRoute);
+router.use('/likes', likeRoute);
+router.use('/reposts', repostRoute);
+router.use('/comments', commentRoute);
+router.use('/follow', followRoute);
+router.use('/dm', dmRoute);
+router.use('/settings', settingsRoute);
+router.use('/status', statusRoute);
+router.use('/explore', exploreRoute);
+
+module.exports = router;
